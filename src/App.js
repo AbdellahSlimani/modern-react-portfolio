b2363b583b8e289efbbee63eb57cc8b7486238a7
Abdellah Slimani <85078022+Slimani-Abdellah@users.noreply.github.com>
2022-08-20 22:25:04 +0100

    Add the about page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import './App.css';
+import './App.scss';
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />} />
+        <Route path="/" element={<Layout />} >
+          <Route index element={<Home />} />
+          <Route path='/about' element={<About />} />
+          {/* <Route path='/contact' element={<Contact />} /> */}
+        </Route>
       </Routes>
     </>
   );
---
+++ src/components/Home/index.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+import Loader from 'react-loaders';
+import { Link } from 'react-router-dom';
+import LogoTitle from '../../assets/images/logo-s.png'
+import AnimatedLetters from '../AnimatedLetters';
+import './index.scss'
+import Logo from './Logo';
+
+const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['l', 'i', 'm', 'a', 'n', 'i']
+  const jobArray = ['w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'p', 'e', 'r' , '.']
+
+  useEffect(() => {
+    setTimeout(() => {
+      return setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
+
+  return (
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i</span>
+            , <br /> 
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+
+            <img src={LogoTitle} alt="developer" />
+            <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15} />
+            <br />
+            <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={22} />
+          </h1>
+          <h2>
+            Frontend Developer / Mobile Developer
+          </h2>
+          <Link to="/contact" className='flat-button'> CONTACT ME</Link>
+        </div>
+        <Logo />
+      </div>
+      <Loader type='pacman' />
+    </>
+  )
+}
+
+export default Home;
