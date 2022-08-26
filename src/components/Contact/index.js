64807198ec371da1fb0809727872c27b323e0ea1
Abdellah Slimani <85078022+Slimani-Abdellah@users.noreply.github.com>
2022-08-26 16:48:17 +0100

    update contact me page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -37,18 +37,20 @@ const Contact = () => {
     <>
       <div className="container contact-page">
         <div className="text-zone">
-          <h1>
-            <AnimatedLetters
-              letterClass={letterClass}
-              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
-              idx={15}
-            />
-          </h1>
-          <p>
-            I am interested in freelance opportunities - especially on ambitious
-            or large projects. However, if you have any other requests or
-            questions, don't hesitate to contact me using below form either.
-          </p>
+          <div className="conatct-text-zone">
+            <h1>
+              <AnimatedLetters
+                letterClass={letterClass}
+                strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+                idx={15}
+              />
+            </h1>
+            <p>
+              I am interested in freelance opportunities - especially on ambitious
+              or large projects. However, if&nbsp;you have any other requests or
+              questions, don't hesitate to contact me using below form either.
+            </p>
+          </div>
           <div className="contact-form">
             <form ref={form} onSubmit={sendEmail}>
               <ul>
