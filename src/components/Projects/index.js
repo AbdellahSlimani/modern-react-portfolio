0d13e9b2f4f5e35dbefce06cdf57b42f52ae4573
Abdellah Slimani <slimaniabdellah.as@gmail.com>
2022-11-20 01:02:03 +0100

    Add typewriter to the title

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,11 +1,19 @@
 import { useEffect, useState } from 'react'
+import { Cursor, useTypewriter } from "react-simple-typewriter";
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
 import projectsData from '../../data/projectsData.json'
 import './index.scss'
 
 const Projects = () => {
-  const projectsTitleKeyWords = ['responsible', 'meaningful', 'beautiful']
+  const [text, count] = useTypewriter({
+    words: [
+      'responsible', 'meaningful', 'beautiful'
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  });
+ /*  const projectsTitleKeyWords = ['responsible', 'meaningful', 'beautiful']
   const [titlep2, setTitlep2] = useState('')
   let index = 0
 
@@ -17,11 +25,11 @@ const Projects = () => {
 
   useEffect(() => {
     change()
-  }, [])
+  }, []) */
 
   const titlep1 = 'I create '
   const titlep1Array = titlep1.split('')
-  let titlep2Array = titlep2.split('')
+  let titlep2Array = text.split('')
   const titlep3 = ' projects'
   const titlep3Array = titlep3.split('')
 
@@ -49,6 +57,7 @@ const Projects = () => {
               idx={15}
               id="to-change"
             />
+            <Cursor cursorColor="#F7AB0A" />
             <AnimatedLetters
               letterClass={letterClass}
               strArray={titlep3Array}
