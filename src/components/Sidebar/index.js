e1d86969bf6fd9b1aa92f89b83d3e568a251cecd
Abdellah Slimani <slimaniabdellah.as@gmail.com>
2022-11-25 21:41:09 +0100

    Fix styles

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,6 @@
 import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
-import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faHome,
